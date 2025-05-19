0b17b296c8cfc3a47a3ff1053e25edc9d3e4f1cb
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-19 12:08:00 -0300

    FPS Parkour

---
+++ Assets/RetartOnFall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetartOnFall : MonoBehaviour
+{
+    public float yFallHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < yFallHeight)
+        {
+            Debug.Log("Me caí");
+            SceneManager.LoadScene("Parkour");
+        }        
+    }
+}
