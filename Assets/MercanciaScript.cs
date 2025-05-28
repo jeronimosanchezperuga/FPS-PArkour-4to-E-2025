8af8f8f6ef5602283bb428ff310e8acc1b49d1bb
micabod <jeronimosanchezperuga@gmail.com>
2025-05-28 16:01:00 -0300

    Colisiones

---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
